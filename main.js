589cb9ceeac3744ea8e83bd9506d6f7d50827907
janroddy <jan.roddy.e@gmail.com>
2020-02-09 19:18:41 -0700

    form:

--- main.js
+++ main.js
@@ -7,3 +7,29 @@ function myFunction() {
     x.className = "menu";
   }
 }
+
+function getFormLength() {
+  var formLength = document.getElementById("contact_form").elements.length;
+  document.getElementById("demo").innerHTML = "Found " + formLength + " elements in the form.";
+}
+
+function getFirstName() {
+  var firstName = document.getElementById("contact_form").elements.namedItem("first_name").value;
+  return firstName;
+}
+
+function displayThankYouMessage() {
+  var name = getFirstName();
+  document.getElementById("demo").innerHTML = "Thanks, " + name +"!";
+}
+
+function getAllElements() {
+  var x = document.getElementById("contact_form");
+  var txt = "";
+  var i;
+
+  for (i = 0; i < x.length; i++) {
+    txt = txt + x.elements[i].value + "<br>";
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
