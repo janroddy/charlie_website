5a76c0ed25e28b6d4d69fee171a543b656a49d4e
janroddy <jan.roddy.e@gmail.com>
2020-02-20 18:34:29 -0700

    php

---
+++ action_page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+ 
+  <body>
+
+    Welcome <?php echo $_POST["first_name"]; ?><br>
+    Your email address is: <?php echo $_POST["user_mail"]; ?>
+
+  </body>
+
+  
+</html>
