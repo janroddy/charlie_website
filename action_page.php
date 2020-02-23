7537a8cd5ecd0904f7c872e2a4773ed84130364f
janroddy <jan.roddy.e@gmail.com>
2020-02-23 16:42:04 -0700

    form

--- action_page.php
+++ action_page.php
@@ -1,7 +1,36 @@
-<!DOCTYPE html>
-
 <html>
- 
+
+  <?php
+    echo $_POST["first_name"]; 
+    echo "<br>";
+    echo $_POST["last_name"];
+    echo "<br>";
+    echo $_POST["user_mail"];
+    echo "<br>";
+    echo $_POST["user_message"];
+    echo "<br>";
+  ?>
+
+  <?php
+    // define variables and set to empty values
+    $name = $email = $gender = $comment = $website = "";
+
+    //check to see if form has been submitted 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = test_input($_POST["name"]);
+      $email = test_input($_POST["email"]);
+      $website = test_input($_POST["website"]);
+      $comment = test_input($_POST["comment"]);
+      $gender = test_input($_POST["gender"]);
+    }
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+  ?>
+
   <body>
 
     Welcome <?php echo $_POST["first_name"]; ?><br>
@@ -9,5 +38,4 @@
 
   </body>
 
-  
 </html>
